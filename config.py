b980b6dce687c36d3d356359104f0ad728a3d472
Amadeus <vasiavasia10000@gmail.com>
2024-08-26 16:17:30 +0300

    Bugs fix!

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DOWNLOAD_DIR = 'downloads'
-TASKS_FILE = '../jsons/tasks.json'
-KEYS_FILE = '../jsons/api_keys.json'
+TASKS_FILE = 'jsons/tasks.json'
+KEYS_FILE = 'jsons/api_keys.json'
 TASK_CLEANUP_TIME = 10  # minutes
 REQUEST_LIMIT = 20 # per TASK_CLEANUP_TIME
 MAX_WORKERS = 4
