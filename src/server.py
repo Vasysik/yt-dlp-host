3af134cba8e094c6277b14f38ca59765ca31fcd6
Amadeus <vasiavasia10000@gmail.com>
2024-09-09 01:16:30 +0300

    Bug fix

--- src/server.py
+++ src/server.py
@@ -105,13 +105,14 @@ def get_file(filename):
                 elif key == 'qualities':
                     qualities = set()
                     for f in data['formats']:
+                        quality = ''
                         if f.get('height') and int(f.get('height')) >= 144:
                             quality = f'{f["height"]}p'
                         if f.get('fps') and int(f.get('fps')) >= 15:
                             quality += str(int(f["fps"]))
                         if quality: qualities.add(quality)
                     filtered_data[key] = sorted(list(qualities), key=lambda x: (int(x.split('p')[0]), int(x.split('p')[1]) if 'p' in x and x.split('p')[1] else 0))
-            
+
             if filtered_data:
                 return jsonify(filtered_data)
             else:
