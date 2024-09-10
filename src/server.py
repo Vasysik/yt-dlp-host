f949a40fc573e794bf0b424cdea93ea46e4512a4
Amadeus <vasiavasia10000@gmail.com>
2024-09-10 21:08:44 +0300

    Final bug fix

--- src/server.py
+++ src/server.py
@@ -113,7 +113,7 @@ def get_file(filename):
                                 "fps": int(f['fps']),
                                 "filesize": int(f.get('filesize') or f.get('filesize_approx') or 0)
                             }
-                    filtered_data[key] = {"qualities": dict(sorted(qualities.items(), key=lambda x: (int(x[0].split('p')[0]), int(x[0].split('p')[1]))))}
+                    filtered_data[key] = sorted(qualities.items(), key=lambda x: (int(x[0].split('p')[0]), int(x[0].split('p')[1])))
 
             if filtered_data:
                 return jsonify(filtered_data)
